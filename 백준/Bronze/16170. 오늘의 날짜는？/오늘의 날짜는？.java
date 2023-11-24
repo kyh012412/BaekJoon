0e5d3a9716557e7d6fac8be01313748a03d04e17
Kwonyounghwan <43436612+kyh012412@users.noreply.github.com>
2023-11-25 08:51:59 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 124 ms, Memory: 13984 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		System.out.println("2023");
+		System.out.println("11");
+		System.out.println("24");
+
+		bw.close();
+		br.close();
+	}
+}
