678fdf0b1ff2803628b7c9db39c0236e2c3f9411
Kwonyounghwan <43436612+kyh012412@users.noreply.github.com>
2023-12-05 09:32:27 +0900

    [Bronze III] Title: 남욱이의 닭장, Time: 144 ms, Memory: 16072 KB -BaekjoonHub

---
+++ 백준/Bronze/11006. 남욱이의 닭장/남욱이의 닭장.java
@@ -0,0 +1,31 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int tc = Integer.parseInt(br.readLine());
+		for(int tci=0;tci<tc;tci++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			int c = 2*b -a;
+			int d = b-c;
+			bw.write(c+" "+d);
+			bw.newLine();
+		}
+
+		bw.close();
+		br.close();
+	}
+}
