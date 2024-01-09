89242f01ecbe0aa812daeedba259442b696fa0cb
Kwonyounghwan <43436612+kyh012412@users.noreply.github.com>
2024-01-09 10:21:33 +0900

    [Bronze I] Title: 2진수 8진수, Time: 2108 ms, Memory: 92280 KB -BaekjoonHub

---
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        BigInteger b = new BigInteger(br.readLine(),2);
+        bw.write(b.toString(8));
+        
+        br.close();
+        bw.close();
+    }
+}
