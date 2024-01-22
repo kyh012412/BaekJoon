d3bd18bd2dd4e45a3955736dca647942e69e6059
Kwonyounghwan <43436612+kyh012412@users.noreply.github.com>
2024-01-22 14:42:06 +0900

    [Bronze III] Title: 사과, Time: 132 ms, Memory: 14328 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10833. 사과/사과.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int ans = 0;
+
+        for(int i=0;i<n;i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            ans += b%a;
+        }
+
+        bw.write(ans+"");
+
+        br.close();
+        bw.close();
+    }
+}
