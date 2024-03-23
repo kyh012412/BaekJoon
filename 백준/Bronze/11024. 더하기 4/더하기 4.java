073af488d1d96a7d6440bf1ce0858e86ce2a3af6
Kwonyounghwan <43436612+kyh012412@users.noreply.github.com>
2024-03-23 10:13:06 +0900

    [Bronze III] Title: 더하기 4, Time: 160 ms, Memory: 14916 KB -BaekjoonHub

---
+++ 백준/Bronze/11024. 더하기 4/더하기 4.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int tc = Integer.parseInt(br.readLine());
+        for(int tci=0;tci<tc;tci++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int sum =0;
+            while(st.hasMoreTokens()) {
+                sum += Integer.parseInt(st.nextToken());
+            }
+            bw.write(sum+"\n");
+        }
+
+        br.close();
+        bw.close();
+    }
+}
