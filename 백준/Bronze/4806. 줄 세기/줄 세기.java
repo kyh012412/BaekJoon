fa06e5ca76724134b2b09a6ff76d9bb320e7cd63
Kwonyounghwan <43436612+kyh012412@users.noreply.github.com>
2024-05-02 21:10:02 +0900

    [Bronze III] Title: 줄 세기, Time: 120 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Bronze/4806. 줄 세기/줄 세기.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int ans = 0;
+		
+		String str;
+		
+		while((str=br.readLine())!=null) {
+			ans++;
+		}
+		bw.write(ans+"");
+		
+		br.close();
+		bw.close();
+	}
+}
