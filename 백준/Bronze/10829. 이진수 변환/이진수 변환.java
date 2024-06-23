a0ab67e089fce2e970571bc2cc4ed79f8dd90c75
Kwonyounghwan <43436612+kyh012412@users.noreply.github.com>
2024-06-24 07:43:39 +0900

    [Bronze II] Title: 이진수 변환, Time: 136 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Bronze/10829. 이진수 변환/이진수 변환.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long a = Long.parseLong(br.readLine());
+
+        String str = Long.toString(a, 2);
+
+        bw.write(str);
+
+        br.close();
+        bw.close();
+    }
+}
