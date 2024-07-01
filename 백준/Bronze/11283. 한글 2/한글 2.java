ef36ef9d732be776cd5bd064866a2e605d18c352
Kwonyounghwan <43436612+kyh012412@users.noreply.github.com>
2024-07-02 08:41:07 +0900

    [Bronze IV] Title: 한글 2, Time: 124 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Bronze/11283. 한글 2/한글 2.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+        System.out.println((int)str.charAt(0)-(int)"가".charAt(0)+1);        
+
+        br.close();
+        bw.close();
+    }
+}
