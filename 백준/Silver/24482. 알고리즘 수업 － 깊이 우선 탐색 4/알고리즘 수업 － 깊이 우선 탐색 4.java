3a50bc80fca1d618550e46053e387922f5e4cda7
Kwonyounghwan <43436612+kyh012412@users.noreply.github.com>
2024-09-28 08:55:33 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 4, Time: 984 ms, Memory: 96736 KB -BaekjoonHub

---
+++ 백준/Silver/24482. 알고리즘 수업 － 깊이 우선 탐색 4/알고리즘 수업 － 깊이 우선 탐색 4.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static StringBuilder sb = new StringBuilder();
+    public static StringTokenizer st;
+    public static StringBuffer ans = new StringBuffer();
+
+    public static boolean[] visited;
+    public static int[] depth;
+    public static ArrayList<Integer>[] list;
+
+    public static void dfs(int s,int curd) {
+//      if(visited[s]) return;
+        visited[s] = true;
+        depth[s]=curd;
+        for(int next : list[s]) {
+            if(!visited[next]) {
+                dfs(next,curd+1);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken()); // 시작 정점
+
+        visited = new boolean[n+1];
+        list = new ArrayList[n+1];
+        depth = new int[n+1];
+        for(int i=1;i<n+1;i++) {
+            list[i] = new ArrayList<Integer>();
+        }
+
+        while(m-->0) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            list[a].add(b);
+            list[b].add(a);
+        }
+
+        Arrays.fill(depth, -1);
+        for(int i=1;i<n+1;i++) {
+            Collections.sort(list[i],Collections.reverseOrder());
+        }
+
+        dfs(r,0);
+
+        for(int i=1;i<n+1;i++) {
+            ans.append(depth[i]+"\n");
+        }
+
+        bw.write(ans.toString());
+
+        bw.close();
+        br.close();
+    }
+}
